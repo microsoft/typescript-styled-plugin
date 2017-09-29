eb855b8ed038673ffd1527348febbb12a2276dce
Matt Bierner <matb@microsoft.com>
2017-09-29 14:05:36 -0700

    Add basic error pinning test

---
+++ e2e/tests/errors.js
@@ -0,0 +1,31 @@
+const assert = require('chai').assert;
+const createServer = require('../server-fixture');
+
+describe('Errors', () => {
+    it('should return error for unknown property', () => {
+        const server = createServer();
+        server.send({
+            command: 'open',
+            arguments: {
+                file: './main.ts',
+                fileContent: 'function css(x) { return x; }; const q = css`boarder: 1px solid black;`',
+                scriptKindName: 'TS'
+            }
+        });
+        server.send({ command: 'semanticDiagnosticsSync', arguments: { file: 'main.ts' } });
+
+        return server.close().then(() => {
+            assert.strictEqual(server.responses.length, 3);
+            const errorResponse = server.responses[2];
+            assert.isTrue(errorResponse.success);
+            assert.strictEqual(errorResponse.body.length, 1);
+            const error = errorResponse.body[0];
+            assert.strictEqual(error.text, 'Unknown property.');
+            assert.strictEqual(error.start.line, 1);
+            assert.strictEqual(error.start.offset, 46);
+            assert.strictEqual(error.end.line, 1);
+            assert.strictEqual(error.end.offset, 53);
+        });
+    });
+
+})
