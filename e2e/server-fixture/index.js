ceff604e5f38c2eae428dac351c4931a1336f2f4
Matt Bierner <matb@microsoft.com>
2017-10-09 18:44:12 -0700

    Use full path for tsserver

--- e2e/server-fixture/index.js
+++ e2e/server-fixture/index.js
@@ -4,14 +4,14 @@ const readline = require('readline');
 
 class TSServer {
     constructor() {
-        const tsserverPath = require.resolve('typescript/lib/tsserver');
+        const tsserverPath = path.join(__dirname, '..', 'node_modules', 'typescript', 'lib', 'tsserver');
         const server = fork(tsserverPath, [
             '--logVerbosity', 'verbose',
             '--logFile', path.join(__dirname, 'log.txt')
         ], {
-            cwd: path.join(__dirname, '../project-fixture'),
-            stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
-        });
+                cwd: path.join(__dirname, '..', 'project-fixture'),
+                stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+            });
         this._exitPromise = new Promise((resolve, reject) => {
             server.on('exit', code => resolve(code));
             server.on('error', reason => reject(reason));
@@ -19,7 +19,7 @@ class TSServer {
         server.stdout.setEncoding('utf-8');
         readline.createInterface({
             input: server.stdout
-        }).on('line', line => {            
+        }).on('line', line => {
             if (line[0] === '{') {
                 this.responses.push(JSON.parse(line));
             }
