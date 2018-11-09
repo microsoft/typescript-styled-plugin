e353d5e2f526f2e8747fe4fa7947201f1a653b57
Matt Bierner <matb@microsoft.com>
2018-11-08 16:26:27 -0800

    Convert some tests to async

--- e2e/tests/errors.js
+++ e2e/tests/errors.js
@@ -2,14 +2,14 @@
 const assert = require('chai').assert;
 const path = require('path');
 const createServer = require('../server-fixture');
-const {openMockFile, getFirstResponseOfType} = require('./_helpers');
+const { openMockFile, getFirstResponseOfType } = require('./_helpers');
 
 const mockFileName = path.join(__dirname, '..', 'project-fixture', 'main.ts');
 
 const getSemanticDiagnosticsForFile = (fileContents) => {
     const server = createServer();
     openMockFile(server, mockFileName, fileContents);
-    server.sendCommand('semanticDiagnosticsSync', {file: mockFileName});
+    server.sendCommand('semanticDiagnosticsSync', { file: mockFileName });
 
     return server.close().then(_ => {
         return getFirstResponseOfType('semanticDiagnosticsSync', server);
@@ -17,132 +17,105 @@ const getSemanticDiagnosticsForFile = (fileContents) => {
 }
 
 describe('Errors', () => {
-    it('should return error for unknown property', () => {
-        return getSemanticDiagnosticsForFile(
-            'function css(x) { return x; }; const q = css`boarder: 1px solid black;`'
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 1);
-            const error = errorResponse.body[0];
-            assert.strictEqual(error.text, "Unknown property: 'boarder'");
-            assert.strictEqual(error.start.line, 1);
-            assert.strictEqual(error.start.offset, 46);
-            assert.strictEqual(error.end.line, 1);
-            assert.strictEqual(error.end.offset, 53);
-        });
+    it('should return error for unknown property', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile('function css(x) { return x; }; const q = css`boarder: 1px solid black;`');
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 1);
+        const error = errorResponse.body[0];
+        assert.strictEqual(error.text, "Unknown property: 'boarder'");
+        assert.strictEqual(error.start.line, 1);
+        assert.strictEqual(error.start.offset, 46);
+        assert.strictEqual(error.end.line, 1);
+        assert.strictEqual(error.end.offset, 53);
     });
 
-    it('should not return errors for empty rulesets', () => {
-        return getSemanticDiagnosticsForFile(
-            'function css(x) { return x; }; const q = css``'
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 0);
-        });
+    it('should not return errors for empty rulesets', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile('function css(x) { return x; }; const q = css``');
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 0);
     });
 
-    it('should not return errors for nested rulesets', () => {
-        return getSemanticDiagnosticsForFile(
-            'function css(x) { return x; }; const q = css`&:hover { border: 1px solid black; }`'
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 0);
-        });
+    it('should not return errors for nested rulesets', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile('function css(x) { return x; }; const q = css`&:hover { border: 1px solid black; }`');
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 0);
     });
 
-    it('should not return an error for a placeholder in a property', () => {
-        return getSemanticDiagnosticsForFile(
-            'function css(strings, ...) { return ""; }; const q = css`color: ${"red"};`'
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 0);
-        });
+    it('should not return an error for a placeholder in a property', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile('function css(strings, ...) { return ""; }; const q = css`color: ${"red"};`');
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 0);
     });
 
-    it('should not return an error for a placeholder in a property with a multiline string', () => {
-        return getSemanticDiagnosticsForFile([
+    it('should not return an error for a placeholder in a property with a multiline string', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile([
             'function css(strings, ...) { return ""; }; const q = css`',
             '    color: ${"red"};',
-            '`'].join('\n')
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 0);
-        });
+            '`'
+        ].join('\n'));
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 0);
     });
 
-    it('should return errors when error occurs in last position', () => {
-        return getSemanticDiagnosticsForFile(
-            'function css(strings, ...) { return ""; }; const q = css`;`'
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 1);
-            const error = errorResponse.body[0];
-            assert.strictEqual(error.text, '} expected');
-            assert.strictEqual(error.start.line, 1);
-            assert.strictEqual(error.start.offset, 58);
-            assert.strictEqual(error.end.line, 1);
-            assert.strictEqual(error.end.offset, 59);
-        });
+    it('should return errors when error occurs in last position', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile('function css(strings, ...) { return ""; }; const q = css`;`');
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 1);
+        const error = errorResponse.body[0];
+        assert.strictEqual(error.text, '} expected');
+        assert.strictEqual(error.start.line, 1);
+        assert.strictEqual(error.start.offset, 58);
+        assert.strictEqual(error.end.line, 1);
+        assert.strictEqual(error.end.offset, 59);
     });
 
-    it('should return error for multiline unknown property #20', () => {
-        return getSemanticDiagnosticsForFile([
+    it('should return error for multiline unknown property #20', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile([
             'function css(x) { return x; };',
             'const q = css`',
             'boarder: 1px solid black;',
             '`'
-        ].join('\n')
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 1);
-            const error = errorResponse.body[0];
-            assert.strictEqual(error.text, "Unknown property: 'boarder'");
-            assert.strictEqual(error.start.line, 3);
-            assert.strictEqual(error.start.offset, 1);
-            assert.strictEqual(error.end.line, 3);
-            assert.strictEqual(error.end.offset, 8);
-        });
+        ].join('\n'));
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 1);
+        const error = errorResponse.body[0];
+        assert.strictEqual(error.text, "Unknown property: 'boarder'");
+        assert.strictEqual(error.start.line, 3);
+        assert.strictEqual(error.start.offset, 1);
+        assert.strictEqual(error.end.line, 3);
+        assert.strictEqual(error.end.offset, 8);
     });
 
-    it('should not error with interpolation at start, followed by semicolon #22', () => {
-        return getSemanticDiagnosticsForFile([
+    it('should not error with interpolation at start, followed by semicolon #22', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile([
             "function css(...args){}",
             "const mixin = ''",
-
             // test single-line
             "css`${mixin}; color: blue;`",
-
             // test multi-line (normal case)
             "css`",
             "  ${mixin};",
             "  color: blue;",
             "`",
-
             // test multiple spaces after semi
             "css`",
             "  ${mixin}   ;",
             "  color: blue;",
             "`",
-
             // test hella semis - will this ever pop up? probably not, but screw it
             "css`",
             "  ${mixin};;; ;; ;",
             "  color: blue;",
             "`",
-        ].join('\n')
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 0);
-        });
+        ].join('\n'));
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 0);
     });
 
-    it('should not return an error for a placeholder used as a selector (#30)', () => {
-        return getSemanticDiagnosticsForFile(
-            'function css(strings, ...) { return ""; }; const q = css`${"button"} { color: red;  }`'
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 0);
-        });
+    it('should not return an error for a placeholder used as a selector (#30)', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile('function css(strings, ...) { return ""; }; const q = css`${"button"} { color: red;  }`');
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 0);
     });
 
     it('should not return an error for a placeholder used as a complex selector (#30)', () => {
@@ -170,13 +143,10 @@ describe('Errors', () => {
         });
     });
 
-    it('should not return an error for a placeholder used as selector part (#39)', () => {
-        return getSemanticDiagnosticsForFile(
-            'function css(strings, ...) { return ""; }; const Content = "button"; const q = css`& > ${Content} { margin-left: 1px; }`'
-        ).then(errorResponse => {
-            assert.isTrue(errorResponse.success);
-            assert.strictEqual(errorResponse.body.length, 0);
-        });
+    it('should not return an error for a placeholder used as selector part (#39)', async () => {
+        const errorResponse = await getSemanticDiagnosticsForFile('function css(strings, ...) { return ""; }; const Content = "button"; const q = css`& > ${Content} { margin-left: 1px; }`');
+        assert.isTrue(errorResponse.success);
+        assert.strictEqual(errorResponse.body.length, 0);
     });
 
     it('should not return an error for a placeholder in multiple properties (#39)', () => {
