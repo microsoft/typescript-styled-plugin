dce60e9dcb39b6c02bcae7089ad761c6200a9027
Matt Bierner <matb@microsoft.com>
2018-11-09 15:25:28 -0800

    Convert to async

--- e2e/tests/emmetCompletions.js
+++ e2e/tests/emmetCompletions.js
@@ -4,7 +4,6 @@ const path = require('path');
 const createServer = require('../server-fixture');
 const { openMockFile, getFirstResponseOfType } = require('./_helpers');
 
-
 const createMockFileForServer = (fileContents, project) => {
     project = project || 'project-fixture'
     const server = createServer(project);
@@ -14,28 +13,26 @@ const createMockFileForServer = (fileContents, project) => {
 }
 
 describe('Emmet Completions', () => {
-    it('shouldnt return emmet property completions when disabled', () => {
-        const { server, mockFileName } = createMockFileForServer('const q = css`m10-20`', 'disabled-emmet-project-fixture');
+    it('shouldnt return emmet property completions when disabled', async () => {
+        const { server, mockFileName } = createMockFileForServer(
+            'const q = css`m10-20`', 'disabled-emmet-project-fixture');
         server.sendCommand('completions', { file: mockFileName, offset: 21, line: 1 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.every(item => item.name !== 'margin: 10px 20px;'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.every(item => item.name !== 'margin: 10px 20px;'));
     });
 
-    it('should return emmet property completions for single line string', () => {
+    it('should return emmet property completions for single line string', async () => {
         const { server, mockFileName } = createMockFileForServer('const q = css`m10-20`');
         server.sendCommand('completions', { file: mockFileName, offset: 21, line: 1 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
     });
 
-
-    it('should return emmet property completions for multiline string', () => {
+    it('should return emmet property completions for multiline string', async () => {
         const { server, mockFileName } = createMockFileForServer([
             'const q = css`',
             'm10-20',
@@ -43,87 +40,81 @@ describe('Emmet Completions', () => {
         ].join('\n'));
         server.sendCommand('completions', { file: mockFileName, offset: 7, line: 2 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
     });
 
-    it('should return emmet property completions for nested selector', () => {
-        const { server, mockFileName } = createMockFileForServer('const q = css`position: relative; &:hover { m10-20 }`');
+    it('should return emmet property completions for nested selector', async () => {
+        const { server, mockFileName } = createMockFileForServer(
+            'const q = css`position: relative; &:hover { m10-20 }`');
+
         server.sendCommand('completions', { file: mockFileName, offset: 51, line: 1 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
     });
 
-    it('should return emmet completions when placeholder is used as property', () => {
+    it('should return emmet completions when placeholder is used as property', async () => {
         const { server, mockFileName } = createMockFileForServer('css`m10-20 ; boarder: 1px solid ${"red"};`');
         server.sendCommand('completions', { file: mockFileName, offset: 11, line: 1 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
     });
 
-    it('should return emmet completions after where placeholder is used as property', () => {
+    it('should return emmet completions after where placeholder is used as property', async () => {
         const { server, mockFileName } = createMockFileForServer('css`border: 1px solid ${"red"}; m10-20`');
         server.sendCommand('completions', { file: mockFileName, offset: 39, line: 1 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === 'margin: 10px 20px;'));
     });
 
-    it('should return emmet completions between were placeholders are used as properties', () => {
+    it('should return emmet completions between were placeholders are used as properties', async () => {
         const { server, mockFileName } = createMockFileForServer('css`boarder: 1px solid ${"red"}; color: #12; margin: ${20}; `')
         server.sendCommand('completions', { file: mockFileName, offset: 44, line: 1 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
     });
 
-    it('should return emmet completions on tagged template string with placeholder using dotted tag', () => {
+    it('should return emmet completions on tagged template string with placeholder using dotted tag', async () => {
         const { server, mockFileName } = createMockFileForServer('css.x`color: #12 ; boarder: 1px solid ${"red"};`');
         server.sendCommand('completions', { file: mockFileName, offset: 17, line: 1 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.success);
-            assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.success);
+        assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
     });
 
-    it('should return styled emmet completions inside of nested placeholder', () => {
+    it('should return styled emmet completions inside of nested placeholder', async () => {
         const { server, mockFileName } = createMockFileForServer('styled`background: red; ${(() => css`color: #12`)()}`;');
         server.sendCommand('completions', { file: mockFileName, offset: 48, line: 1 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
     });
 
-    it('should handle emmet completions in multiline value placeholder correctly ', () => {
+    it('should handle emmet completions in multiline value placeholder correctly ', async () => {
         const { server, mockFileName } = createMockFileForServer([
             'css`margin: ${',
             '0',
             "}; color: #12`"].join('\n'));
         server.sendCommand('completions', { file: mockFileName, offset: 14, line: 3 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
     });
 
-    it('should handle emmet completions in multiline rule placeholder correctly ', () => {
+    it('should handle emmet completions in multiline rule placeholder correctly ', async () => {
         const { server, mockFileName } = createMockFileForServer([
             'css`',
             '${',
@@ -132,13 +123,12 @@ describe('Emmet Completions', () => {
             'color: #12`'].join('\n'));
         server.sendCommand('completions', { file: mockFileName, offset: 11, line: 5 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
     });
 
-    it('should return emmet completions inside of nested selector xx', () => {
+    it('should return emmet completions inside of nested selector xx', async () => {
         const { server, mockFileName } = createMockFileForServer([
             'css`',
             '    color: red;',
@@ -148,9 +138,8 @@ describe('Emmet Completions', () => {
             '`'].join('\n'));
         server.sendCommand('completions', { file: mockFileName, line: 4, offset: 19 });
 
-        return server.close().then(() => {
-            const completionsResponse = getFirstResponseOfType('completions', server);
-            assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
-        });
+        await server.close();
+        const completionsResponse = getFirstResponseOfType('completions', server);
+        assert.isTrue(completionsResponse.body.some(item => item.name === '#121212'));
     });
 });
